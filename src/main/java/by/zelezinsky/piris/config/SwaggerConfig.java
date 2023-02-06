9218164da64cd3e697a8afc7495b9bfb4148a86f
Ivan Zeliazinski <s1mpleknight@mail.ru>
2023-02-07 02:07:18 +0300

    new: add cors configs

--- src/main/java/by/zelezinsky/piris/config/SwaggerConfig.java
+++ src/main/java/by/zelezinsky/piris/config/SwaggerConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class GlobalConfiguration {
+public class SwaggerConfig {
 
     @Bean
     public OpenAPI springShopOpenAPI() {
